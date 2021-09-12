2695c1d786d5e45abc419a660736cadfd1c30cc5
jntun <justin@jntun.com>
2021-09-12 03:47:35 -0700

    Frontend: Add state for submissions and use Submission component to render data

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,20 +1,22 @@
 import logo from './logo.svg';
 import React from 'react';
 import axios from 'axios';
+import Submission from "./Submission";
 import './App.css';
 
 class App extends React.Component {
     constructor(props) {
         super(props);
 
-        this.state = {submissions: false}
+        this.state = {submissions: [], data: false, maxView: 10}
     }
 
     getSubmissions = () => {
         axios.get('http://localhost:8000/battle/submissions')
             .then((response) => {
                 if (response.status === 200) {
-                    this.setState({submissions: JSON.parse(response.data)})
+                    let submissions = Object.keys(response.data);
+                    this.setState({submissions: submissions, data: true});
                 }
             })
             .catch((err) => {
@@ -28,12 +30,17 @@ class App extends React.Component {
     }
 
     render() {
-        console.log(this.state.submissions);
-        let submissions;
-        if (!submissions) {
+        let submissions = [];
+        if (!this.state.data) {
             submissions = <p id={"no-submissions"}>No submissions.</p>
         } else {
-            submissions = this.state.submissions;
+            for (let i = 0; i < this.state.submissions.length; i++) {
+                if (i === this.state.maxView) {
+                    break;
+                }
+                let x = this.state.submissions[i];
+                submissions.push(<Submission id={x}/>)
+            }
         }
 
 
--- frontend/src/Submission.js
+++ frontend/src/Submission.js
@@ -1,10 +1,26 @@
 import React from 'react';
+import axios from 'axios';
 
 export default class Submission extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {data: props.data};
+    }
+
+    componentDidMount() {
+        axios.get("localhost:8000/battle/submission/" + this.props.id)
+            .then((response) => {
+                if (response.status === 200) {
+                    this.setState({data: response.data})
+                }
+            })
+    }
+
     render() {
         return (
             <div id={this.props.id} className={"submission"}>
-                <p>submission</p>
+                <p>Submission#{this.props.id}</p>
             </div>
         )
     }
