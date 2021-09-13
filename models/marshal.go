292e21e214e20798a993c1cdb377f8da6650c3da
jntun <justin@jntun.com>
2021-09-13 11:08:26 -0700

    Models: Add MarshalJSON() implementation for Submission type

--- models/marshal.go
+++ models/marshal.go
@@ -1,1 +1,35 @@
 package models
+
+import (
+	"bytes"
+	"encoding/json"
+	"io"
+)
+
+func (subm Submission) MarshalJSON() ([]byte, error) {
+	buff := bytes.NewBuffer([]byte{})
+	buff.Write([]byte("{"))
+	//addKeyValTrail(buff, "uuid", subm.Author.ID.String())
+	addKeyValTrail(buff, "votes", subm.Votes)
+	addKeyValTrail(buff, "type", subm.Type)
+	addKeyVal(buff, "resource", subm.Resource.String())
+	buff.Write([]byte("}"))
+
+	return buff.Bytes(), nil
+}
+
+func addKeyValTrail(w io.Writer, key string, val interface{}) {
+	addKeyVal(w, key, val)
+	w.Write([]byte(","))
+}
+
+func addKeyVal(w io.Writer, key string, val interface{}) {
+	valBytes, err := json.Marshal(val)
+	if err != nil {
+		// fail
+	}
+	w.Write([]byte("\""))
+	w.Write([]byte(key))
+	w.Write([]byte("\":"))
+	w.Write(valBytes)
+}
