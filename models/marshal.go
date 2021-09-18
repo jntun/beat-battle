a0328712d1da1aa15040347d5cdd621af8f1f6ef
jntun <justin@jntun.com>
2021-09-18 08:33:40 -0700

    Models: Add AsJSONBytes func for ResponseStatus
    This is for quickly turning a ResponseStatus into json marshaled bytes for writing.

--- models/marshal.go
+++ models/marshal.go
@@ -4,18 +4,28 @@ import (
 	"bytes"
 	"encoding/json"
 	"io"
+	"log"
 )
 
 func (subm Submission) MarshalJSON() ([]byte, error) {
 	buff := bytes.NewBuffer([]byte{})
 	buff.Write([]byte("{"))
 	//addKeyValTrail(buff, "uuid", subm.Author.ID.String())
-	addKeyValTrail(buff, "votes", subm.Votes)
+	//addKeyValTrail(buff, "votes", subm.Votes)
 	addKeyValTrail(buff, "type", subm.Type)
 	addKeyVal(buff, "resource", subm.Resource.String())
 	buff.Write([]byte("}"))
 
 	return buff.Bytes(), nil
+}
+
+func (resp ResponseStatus) AsJSONBytes() []byte {
+	bytez, err := json.Marshal(resp)
+	if err != nil {
+		log.Printf("[FAIL] ResponseStatus.AsJSONBytes: %s", err)
+		return []byte("")
+	}
+	return bytez
 }
 
 func addKeyValTrail(w io.Writer, key string, val interface{}) {
